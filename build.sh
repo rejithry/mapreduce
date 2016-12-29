041a89c7b5530fd74cd945fa0971bc377db3f9a8
Rejith Raghavan <rejith.raghavan@disney.com>
2016-12-28 16:02:54 -0800

    added common friends

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ cd /usr/local/hadoop
 bin/hadoop fs -copyFromLocal /top10.txt / || true
 bin/hadoop fs -copyFromLocal /url.txt / || true
 bin/hadoop fs -copyFromLocal /index.txt / || true
+bin/hadoop fs -copyFromLocal /friends.txt / || true
 
 bin/hadoop fs -rm -r /testoutput || true
 bin/hadoop fs -rm -r /tmp/pagerank || true
